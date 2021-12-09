7b79f4390982ecbb385f174cba06f059280858a9
Kevin Schweitzer <kevschweitzer@gmail.com>
2021-12-09 00:40:28 -0300

    Move the getFrequentRenterPoints method (again)

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -38,4 +38,14 @@ public class Movie {
         }
         return result;
     }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        // add frequent renter points
+        int frequentRenterPoints = 1;
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            frequentRenterPoints ++;
+
+        return frequentRenterPoints;
+    }
 }
