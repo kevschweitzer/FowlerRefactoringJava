217301da18d6c81ef196d833311c49c384e88454
Kevin Schweitzer <kevschweitzer@gmail.com>
2021-12-08 23:32:56 -0300

    Extracting Frequent Renter Points

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -32,4 +32,14 @@ class Rental {
         }
         return result;
     }
+
+    public int getFrequentRenterPoints() {
+        // add frequent renter points
+        int frequentRenterPoints = 1;
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+            frequentRenterPoints ++;
+
+        return frequentRenterPoints;
+    }
 }
