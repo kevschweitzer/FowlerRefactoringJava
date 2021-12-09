20d490b7f10b987bed043bd42dc5f4d45d06ccc2
Kevin Schweitzer <kevschweitzer@gmail.com>
2021-12-09 01:09:02 -0300

    Replace conditional with Polymorphism

--- src/main/java/NewReleasePrice.java
+++ src/main/java/NewReleasePrice.java
@@ -9,4 +9,9 @@ public class NewReleasePrice extends Price{
     double getCharge(int daysRented) {
         return daysRented * 3;
     }
+
+    @Override
+    int getFrequentRenterPoints(int daysRented) {
+        return  (daysRented > 1) ? 2 : 1;
+    }
 }
