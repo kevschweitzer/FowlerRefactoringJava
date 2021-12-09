d955dfdb075ca40b0cdfd600516e05a869664eed
Kevin Schweitzer <kevschweitzer@gmail.com>
2021-12-09 01:02:51 -0300

    Replace conditional with Polymorphism

--- src/main/java/ChildrenPrice.java
+++ src/main/java/ChildrenPrice.java
@@ -4,4 +4,13 @@ public class ChildrenPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDRENS;
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+
+        return result;
+    }
 }
